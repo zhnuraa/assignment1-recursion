b4060a2725754d404a48675a80349363610d5531
zhnuraa <250822@astanait.edu.kz>
2026-03-22 22:17:09 +0500

    Implement recursive power function in Task6

---
+++ #6.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task6 {
+    static int power(int a, int n) {
+        if (n == 0) return 1;
+        return a * power(a, n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println(power(a, n));
+    }
+}
