ce4d5da6259409aba5371c24f16527ad6c264b7a
zhnuraa <250822@astanait.edu.kz>
2026-03-22 22:16:42 +0500

    Implement Fibonacci sequence calculation

---
+++ #5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task5 {
+    static int fibonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fibonacci(n));
+    }
+}
